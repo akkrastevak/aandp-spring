3d2e68f7617a7122b525da94562d1154d994951f
akkrastevak <atanas.krastev@abv.bg>
2021-12-19 13:35:13 +0200

    initial commit

---
+++ src/main/java/com/aandp/aandpspring/AandpSpringApplication.java
@@ -0,0 +1,13 @@
+package com.aandp.aandpspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AandpSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AandpSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aandp/aandpspring/AandpSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aandp.aandpspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AandpSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
